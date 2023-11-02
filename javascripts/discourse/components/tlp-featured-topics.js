78a12bde840891448e1ed8ee626b3ff2041c5d3f
merefield <merefield@gmail.com>
2023-11-02 21:58:55 +0800

    COMPATIBILITY: remove route edits to support Ember upgrade

--- javascripts/discourse/components/tlp-featured-topics.js
+++ javascripts/discourse/components/tlp-featured-topics.js
@@ -1,12 +1,16 @@
 import { cook } from 'discourse/lib/text';
-import { computed } from "@ember/object";
+import { action, computed } from "@ember/object";
 import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking";
 import { inject as service } from "@ember/service";
+import {findOrResetCachedTopicList} from 'discourse/lib/cached-topic-list';
 
 export default class TlpFeaturedTopicsComponent extends Component {
   @service appEvents;
+  @service store;
+  @service session;
   @tracked featuredTitle = "";
+  @tracked featuredTopics = [];
 
   constructor() {
     super(...arguments);
@@ -15,12 +19,44 @@ export default class TlpFeaturedTopicsComponent extends Component {
     if (this.showFeaturedTitle) {
       const raw = settings.topic_list_featured_title;
       cook(raw).then((cooked) => this.featuredTitle = cooked);
-    }
+    };
   };
 
-  @computed
-  get hasTopics() {
-    return this.args.featuredTopics.length > 0;
+  @action
+  async getFeaturedTopics() {
+    let topics = [];
+    let filter = `tag/${settings.topic_list_featured_images_tag}`;
+    let lastTopicList = findOrResetCachedTopicList (this.session, filter);
+
+    list = await this.store.findFiltered ('topicList', {filter} )
+    
+    topics = Ember.Object.create(list).topic_list.topics;
+  
+    if (this.args.category && settings.topic_list_featured_images_from_current_category_only) {
+      topics = topics.filter(topic => topic.category_id == this.args.category.id)
+    };
+  
+    const reducedTopics = topics ? settings.topic_list_featured_images_count == 0 ? topics : topics.slice(0,settings.topic_list_featured_images_count) : [];
+
+    if (settings.topic_list_featured_images_created_order) {
+      reducedTopics.sort((a, b) => {
+        var keyA = new Date(a.created_at), keyB = new Date(b.created_at);
+        // Compare the 2 dates
+        if (keyA < keyB) return 1;
+        if (keyA > keyB) return -1;
+        return 0;
+      });
+    }
+    this.featuredTopics = reducedTopics;
+  };
+
+  @computed("featuredTopics")
+  get showFeatured() {
+    return (settings.topic_list_featured_images && 
+    this.args.category == null ||
+    settings.topic_list_featured_images_category &&
+    this.args.category !== null) &&
+    this.featuredTopics.length > 0;
   };
 
   @computed
