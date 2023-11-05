6bca11e7fb5568636c2fa9387f64a561f9aa5a37
merefield <merefield@gmail.com>
2023-11-05 13:07:53 +0000

    FIX: cases where featured tag not set and ma ke sure list variable is instantiated

--- javascripts/discourse/components/tlp-featured-topics.js
+++ javascripts/discourse/components/tlp-featured-topics.js
@@ -25,29 +25,32 @@ export default class TlpFeaturedTopicsComponent extends Component {
   @action
   async getFeaturedTopics() {
     let topics = [];
-    let filter = `tag/${settings.topic_list_featured_images_tag}`;
-    let lastTopicList = findOrResetCachedTopicList (this.session, filter);
+    if (settings.topic_list_featured_images_tag !== "") {
+      let filter = `tag/${settings.topic_list_featured_images_tag}`;
+      let lastTopicList = findOrResetCachedTopicList (this.session, filter);
+      let list = await this.store.findFiltered ('topicList', {filter} );
 
-    list = await this.store.findFiltered ('topicList', {filter} )
-    
-    topics = Ember.Object.create(list).topic_list.topics;
-  
-    if (this.args.category && settings.topic_list_featured_images_from_current_category_only) {
-      topics = topics.filter(topic => topic.category_id == this.args.category.id)
-    };
-  
-    const reducedTopics = topics ? settings.topic_list_featured_images_count == 0 ? topics : topics.slice(0,settings.topic_list_featured_images_count) : [];
+      if (typeof list !== 'undefined') {
+          topics = Ember.Object.create(list).topic_list.topics;
 
-    if (settings.topic_list_featured_images_created_order) {
-      reducedTopics.sort((a, b) => {
-        var keyA = new Date(a.created_at), keyB = new Date(b.created_at);
-        // Compare the 2 dates
-        if (keyA < keyB) return 1;
-        if (keyA > keyB) return -1;
-        return 0;
-      });
+        if (this.args.category && settings.topic_list_featured_images_from_current_category_only) {
+          topics = topics.filter(topic => topic.category_id == this.args.category.id)
+        };
+
+        const reducedTopics = topics ? settings.topic_list_featured_images_count == 0 ? topics : topics.slice(0,settings.topic_list_featured_images_count) : [];
+
+        if (settings.topic_list_featured_images_created_order) {
+          reducedTopics.sort((a, b) => {
+            var keyA = new Date(a.created_at), keyB = new Date(b.created_at);
+            // Compare the 2 dates
+            if (keyA < keyB) return 1;
+            if (keyA > keyB) return -1;
+            return 0;
+          });
+        }
+        this.featuredTopics = reducedTopics;
+      }
     }
-    this.featuredTopics = reducedTopics;
   };
 
   @computed("featuredTopics")
