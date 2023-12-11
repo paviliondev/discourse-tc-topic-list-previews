5209a1aab7b26c6fe7be4cabf39868584a6585a4
merefield <merefield@gmail.com>
2023-12-11 09:01:36 +0000

    Revert "Revert "Move thumbnail selector to Glimmmer to resolve deprecations""
    This reverts commit 969192e7f6752cb70ea982204ef66fd1fb23c766.

---
+++ javascripts/discourse/components/select-thumbnail.js
@@ -0,0 +1,31 @@
+import { ajax } from 'discourse/lib/ajax';
+import { popupAjaxError } from 'discourse/lib/ajax-error';
+import { action, computed } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Component from '@glimmer/component';
+import TlpThumbnailSelectorModalComponent from "../components/modal/tlp-thumbnail-selector";
+
+export default class SelectThumbnailComponent extends Component {
+  @service modal;
+
+  @computed
+  get showSelected() {
+    return this.args.buffered.user_chosen_thumbnail_url ? true : false;
+  };
+
+  @action
+  showThumbnailSelector() {
+    ajax(`/topic-previews/thumbnail-selection.json?topic=${this.args.topic_id}`).then(result => {
+      this.modal.show(TlpThumbnailSelectorModalComponent, {
+        model: {
+          thumbnails: result.thumbnailselection,
+          topic_id: this.args.topic_id,
+          topic_title: this.args.topic_title,
+          buffered: this.args.buffered
+        }
+      })
+    }).catch(function(error) {
+      popupAjaxError(error);
+    });
+  }
+}
